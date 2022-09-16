b32350c7c3ae7f775586a661b281f27361a6f288
01Murod01 <01myoldoshev01@gmail.com>
2022-09-16 21:34:48 +0500

    lesson3

---
+++ main.js
@@ -0,0 +1,146 @@
+// 1 misol
+// let A = 10;
+// if (A > 0) {
+//   console.log("kiritilgan son musbat");
+// } else {
+//   console.log("kiritilgan son 0 yoki manfiy");
+// }
+
+// 2 misol
+// let A = 11;
+// if (A % 2 == 1){
+// console.log("kiritilgan son toq son");
+// }else{
+// console.log("kiritilgan son juft");
+// }
+
+// 3 misol
+// let A = 10;
+// if (A % 2 == 0){
+// console.log("kiritilgan son juft son");
+// }else{
+// console.log("kiritilgan son toq");
+// }
+
+// 4 misol
+// let a = 10,
+//   b = 20;
+// if(a > 0 && b > 0){
+//     console.log("ikkala son ham 0 dan katta")
+// }else{
+//     console.log("xato")
+// }
+
+// 5 misol
+// let a = 10,
+//   b = 11,
+//   c = 8;
+//   if(a > b && b > c){
+//     console.log("shart to'g'ri bajarildi")
+//   }else if(a > b){
+//     console.log("1-shart qanoatlantirdi")
+//   }else if(b > c){
+//     console.log("2-shart qanoatlantirdi")
+//   }else{
+//     console.log("shart xato")
+//   }
+
+// 6 misol
+// let a = 3,
+//   b = 2,
+//   c = 1;
+// if (a > b && b > c) {
+//   console.log("shart togri");
+// } else {
+//   console.log("shart notogri");
+// }
+
+// 7 misol
+// let a=10, b=11;
+// if(a%2==1&&b%2==1){
+//     console.log("ikkalasi toq")
+// }else if(a%2==1||b%2==1){
+//     console.log("bittasi toq")
+// }else{
+//     console.log("ikkalasi juft")
+// }
+
+// 8 misol
+// let a = -10,
+//   b = 15,
+//   c = 21,
+//   d = 5;
+// if (a > 0 && b > 0 && c > 0 && d > 0) {
+//   console.log("barchasi musbat");
+// } else if (a > 0 || b > 0 || c > 0 || d > 0) {
+//   console.log("musbat son bor lekin 4ta emas");
+// } else {
+//   console.log("musbat yoq
+
+// 9 misol
+// let a = 10,
+//   b = 20,
+//   c = -30;
+// if (a > 0 && b > 0) {
+//   console.log("a va b musbat");
+// } else if (b > 0 && c > 0) {
+//   console.log("c d musbat");
+// } else {
+//   console.log("xato");
+// }
+
+// 10 misol
+// let haftaKunlar = prompt("hafta kunlaridan son kiriting")
+// switch (haftaKunlar){
+//   case "1":
+//     console.log("Dushanba");
+//     break;
+//   case "2":
+//     console.log("Seshanba");
+//     break;
+//   case "3":
+//     console.log("Chorshanba");
+//     break;
+//   case "4":
+//     console.log("Payshanba");
+//     break;
+//   case "5":
+//     console.log("Juma");
+//     break;
+//   case "6":
+//     console.log("shanba");
+//     break;
+//   case "7":
+//     console.log("Yakshanba");
+//     break;
+//   default:
+//     console.log("bunday hafta kuni yoq");
+// }
+
+// 11 misol
+// let telNomer = prompt("telefon kompaniyasi kodini kiriting");
+// switch (telNomer) {
+//   case "90":
+//     console.log("Beeline");
+//     break;
+//   case "91":
+//     console.log("Beeline");
+//     break;
+//   case "93":
+//     console.log("Ucell");
+//     break;
+//   case "94":
+//     console.log("Ucell");
+//     break;
+//   case "95":
+//     console.log("uzmobile");
+//     break;
+//   case "97":
+//     console.log("Mobiuz");
+//     break;
+//   case "98":
+//     console.log("perfectum");
+//     break;
+//   default:
+//     console.log("bunday kompaniya yoq");
+// }
